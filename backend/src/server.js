5b24f0b46413d01a5f09ff5b7a9d41ac825b3297
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-11-30 22:39:15 +0530

    Sign up Endpoints

--- backend/src/server.js
+++ backend/src/server.js
@@ -7,7 +7,8 @@ import { connectDB } from "./lib/db.js";
 const app = express();
 const PORT = process.env.PORT;
 
-app.use("/auth" , authRoutes);
+app.use(express.json());
+app.use("/api/auth" , authRoutes);
 
 app.listen(PORT , () => {
     console.log(`Server is running on ${PORT}`);
