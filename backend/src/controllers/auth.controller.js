180f32f6e805df4484b57af37ffd908ab43bf745
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-12-02 21:56:21 +0530

    Stream User Creation

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { upsertStreamUser } from "../lib/stream.js";
 import User from "../models/User.js";
 import jwt from "jsonwebtoken";
 
@@ -39,7 +40,19 @@ export async function signup(req, res) {
         fullName,
         password,
         profilePic : randomAvatar,
-    })
+    });
+
+    try {
+
+      await upsertStreamUser({
+      id: newUser._id.toString(),
+      name : newUser.fullName,
+      image : newUser.profilePic || ""
+    });
+      console.log(`Stream User Created for : ${newUser.fullName}`);
+    } catch (error) {
+      console.log("Error Creating Stream User : ",error);
+    }
 
     const token = jwt.sign({userId : newUser._id} , process.env.JWT_SECRET_KEY , {
         expiresIn : "7d"
