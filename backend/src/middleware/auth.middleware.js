0b7ad8bfb19127709a6db23d98cc53389d178917
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-12-08 21:39:58 +0530

    ProtectRoute Middlewares for Onboarding

---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,33 @@
+import jwt from "jsonwebtoken";
+import User from "../models/User.js";
+
+
+export const protectRoute = async(req , res , next)=> {
+    try {
+        const token = req.cookie.jwt;
+
+        if(!token)
+        {
+            res.status(401).json({message : "Unauthorized - No token provided"});
+        }
+
+        const decoded = jwt.verify(token , process.env.JWT_SECRET_KEY)
+        if(!decoded)
+        {
+            res.status(401).json({message: "Unauthorized - Invalid token"});
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+        if(!user)
+        {
+            res.status(401).json({message:"Unauthorized - User Not found"})
+        }
+
+        req.user = user;
+        next();
+
+    } catch (error) {
+        console.log("Error in Protect Route MiddleWare");
+        res.status(500).json({message:"Internal Server Error"})
+    }
+}
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,6 +1,8 @@
 
 import express from "express"
 import { login, logout, onboard, signup } from "../controllers/auth.controller.js";
+import { protectRoute } from "../middleware/auth.middleware.js";
+
 const router = express.Router();
 
 
@@ -10,7 +12,7 @@ router.post("/logout" , logout);
 
 router.post("/signup" , signup);
 
-router.post("/onboarding" , onboard);
+router.post("/onboarding" , protectRoute , onboard);
 
 
 export default router;
