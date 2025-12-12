006e13ec584de503f1ef1da18cc32a5054edd16c
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-12-12 23:24:46 +0530

    Onboarding Page - Mutation Function Creation and Backend API request function definition

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -10,3 +10,8 @@ export const getAuthUser = async () => {
     return res.data;
 };
 
+export const completeOnboarding = async(userData) => {
+    const response = await axiosInstance.post("/auth/onboarding" , userData);
+    return response.data;
+}
+
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,6 +1,37 @@
-import React from 'react'
+import { useState } from "react";
+import useAuthUser from "../hooks/useAuthUser.js"
+import { useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import { completeOnboarding } from "../lib/api.js";
 
 const OnboardingPage = () => {
+
+  const {authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+
+
+  const [formState, setFormState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio || "",
+    nativeLanguage: authUser?.nativeLanguage || "",
+    learningLanguage: authUser?.learningLanguage || "",
+    location: authUser?.location || "",
+    profilePic: authUser?.profilePic || "",
+  });
+
+  const {mutate : onboardingMutation , isPending} = useMutation({
+    mutationFn : completeOnboarding,
+    onSuccess: () => {
+      toast.success("Profile Onboarding Successful");
+      queryClient.invalidateQueries({queryKey : ["authUser"]});
+    }
+  })
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    onboardingMutation(formState);
+  }
+
   return (
     <div>OnboardingPage</div>
   )
