bb90ce2246b5d50cadb992a74cf8edbc2f187944
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-12-16 22:23:13 +0530

    Home Page- Required UseQueries and Mutation function Implementation

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,6 +1,42 @@
-import React from 'react'
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { useEffect, useState } from "react";
+import { getOutgoingFriendReqs, getRecommendedUsers, getUserFriends, sendFriendRequest } from "../lib/api.js";
 
 const HomePage = () => {
+  const queryClient =  useQueryClient();
+  const [outgoingRequestsIds , setOutgoingRequestsIds] = useState(new Set());
+
+  const {data : friends = [] , isLoading : loadingFriends} = useQuery({
+    queryKey : ["friends"],
+    queryFn : getUserFriends
+  })
+
+  const {data : recommendedUsers = [] , isLoading: loadingUsers} = useQuery({
+    queryKey : ["users"],
+    queryFn : getRecommendedUsers 
+  })
+
+  const { data: outgoingFriendReqs } = useQuery({
+    queryKey: ["outgoingFriendReqs"],
+    queryFn: getOutgoingFriendReqs,
+  });
+
+  const {mutate : sendRequestMutation , isPending} = useMutation({
+    mutationFn : sendFriendRequest ,
+    onSuccess : () => queryClient.invalidateQueries({queryKey : ["outgoingFriendReqs"]}), 
+  })
+
+  useEffect(() => {
+    const outgoingIds = new Set();
+    if (outgoingFriendReqs && outgoingFriendReqs.length > 0) {
+      outgoingFriendReqs.forEach((req) => {
+        outgoingIds.add(req.recipient._id);
+      });
+      setOutgoingRequestsIds(outgoingIds);
+    }
+  } , [outgoingRequestsIds]);
+
+  
   return (
     <div>HomePage</div>
   )
--- frontend/src/store/useThemeStore.js
+++ frontend/src/store/useThemeStore.js
@@ -3,8 +3,9 @@ import { create } from 'zustand'
 export const useThemeStore = create((set) => ({
   theme :  localStorage.getItem("streamify-theme") ||"coffee",
   setTheme : (theme) => 
-    {
-        localStorage.setItem("streamify-theme",theme);
-        set({theme});
-    },
+  {
+    localStorage.setItem("streamify-theme",theme);
+    set({theme});
+  },
 }))
+
