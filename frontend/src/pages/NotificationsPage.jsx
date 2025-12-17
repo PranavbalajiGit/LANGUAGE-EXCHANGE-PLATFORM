8c1a58c5e1b699b688bbe3f83d57ceddeeccd801
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-12-17 18:27:00 +0530

    Notifications Page - UseQuery and Mutation function implementation

--- frontend/src/pages/NotificationsPage.jsx
+++ frontend/src/pages/NotificationsPage.jsx
@@ -1,4 +1,27 @@
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
+import { getFriendRequests } from "../lib/api.js";
+
 const NotificationsPage = () => {
+
+  const queryClient = useQueryClient();
+
+  const {data : friendRequests , isLoading} = useQuery({
+    queryKey : ["friendRequests"],
+    queryFn : getFriendRequests 
+  })
+
+  const {mutate : acceptRequestMutation , isPending} = useMutation({
+    mutationFn : acceptFriendRequest,
+    onSuccess : () => {
+      queryClient.invalidateQueries({queryKey : ["friendRequests"]})
+      queryClient.invalidateQueries({queryKey : ["friends"]});
+    }
+  })
+
+  const incomingRequests = friendRequests?.incomingReqs || [];
+  const acceptedRequests = friendRequests?.acceptedReqs || [];
+
+
   return (
     <div>NotificationsPage</div>
   )
