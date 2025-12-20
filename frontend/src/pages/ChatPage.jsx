a1c636c44ece1d45db19136df8345125b76e58bf
PranavbalajiGit <pranavbalaji65690@gmail.com>
2025-12-20 19:04:10 +0530

    ChatPage - Stream Chat Creation Implementation

--- frontend/src/pages/ChatPage.jsx
+++ frontend/src/pages/ChatPage.jsx
@@ -1,6 +1,79 @@
-import React from 'react'
+import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
+import useAuthUser from '../hooks/useAuthUser.js';
+import { useQuery } from '@tanstack/react-query';
+import { getStreamToken } from '../lib/api.js';
+
+import {
+  Channel,
+  ChannelHeader,
+  Chat,
+  MessageInput,
+  MessageList,
+  Thread,
+  Window,
+} from "stream-chat-react";
+import { StreamChat } from 'stream-chat';
+
+import toast from 'react-hot-toast'
+import ChatLoader from '../components/ChatLoader.jsx';
+
+const STREAM_API_KEY = import.meta.env.VITE_STREAM_API_KEY
 
 const ChatPage = () => {
+
+  const {id : targetUserId} = useParams();
+
+  const [chatClient , setChatClient] = useState(null);
+  const [channel , setChannel] = useState(null);
+  const [loading , setLoading] = useState(true);
+
+  const {authUser} = useAuthUser();
+
+  const {data:tokenData} = useQuery({
+    queryKey : ["streamToken"],
+    queryFn : getStreamToken,
+    enabled : !!authUser // This function will run only when authUser is available.
+  });
+
+  useEffect(() => {
+    const initChat = async() => {
+      if(!tokenData?.token || !authUser) return;
+
+      try {
+        console.log("Initializing Stream Chat. . . .");
+
+        const client = StreamChat.getInstance(STREAM_API_KEY);
+
+        await client.connectUser({
+          id : authUser._id,
+          name : authUser.fullName,
+          image : authUser.profilePic,
+        }, tokenData.token)
+
+        const channelId = [authUser._id , targetUserId].sort().join("-");
+
+        const currChannel = client.channel("messaging" , channelId , {
+          members: [authUser._id , targetUserId],
+        })
+
+        await currChannel.watch();
+
+        setChatClient(client);
+        setChannel(currChannel);
+
+      } catch (error) {
+        console.error("Error initializing chat : " , error);
+        toast.error("Could not connect to the chat.Please try again");
+        
+      } finally {
+        setLoading(false);
+      }
+    }
+  } , [])
+
+  if(loading || !chatClient || !channel) return <ChatLoader />
+  
   return (
     <div>ChatPage</div>
   )
